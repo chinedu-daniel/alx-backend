2948f297b1677cd32975f03fe10a3d1acb843600
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-04 23:37:44 +0100

    100-lfu_cache.py

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Basic dictionary
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """
+    Basic Dictionary
+    """
+
+    def __init__(self):
+        """INIT"""
+        super().__init__()
+        self.current = None
+
+    def put(self, key, items):
+        """
+        check if the key and item are none or not
+        """
+        if key is None or items is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = items
+            self.current = key
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if self.current is not None:
+                self.cache_data.pop(self.current)
+                print(f"DISCARD: {self.current}")
+
+        self.current = key
+        self.cache_data[key] = items
+
+    def get(self, key):
+        """
+        check if key is none or not and exists in cache_dataa
+        """
+        if key is None:
+            return
+
+        if key is self.cache_data:
+            self.current = key
+            return self.cache_data[key]
+
+        return None
