b7652602b4a407077960ca79753edf4d94396037
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-04 14:01:43 +0100

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,23 +3,34 @@
 Basic dictionary
 """
 
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class BasicCache(BaseCaching):
     """
     Basic Dictionary
     """
 
+    def __init__(self):
+        """INIT"""
+        super().__init__()
+
     def put(self, key, item):
         """
         check if the key and item are none or not
         """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        if key is None and item is None:
+            return
+        self.cache_data[key] = items
 
     def get(self, key):
         """
         check if key is none or not and exists in cache_dataa
         """
-        if key is None or key not in self.cache_data:
-            return None
-        return seelf.cache_data[key]
+        if key is None:
+            return
+        value = self.cache_data.get(key, None)
+        if value is None:
+            return
+
+        return value
