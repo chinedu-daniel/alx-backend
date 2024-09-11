66d054d170363aaa366880647d6cb1aa8a4c92bd
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-11 08:41:17 +0100

    2-app.py

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+from flask import Flask, render_template, g, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config:
+    """
+    creating a locale
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+    @babel.localeselector
+    def get_locale():
+        """
+        use the locale from the user settings
+        """
+        return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=["GET"])
+def home():
+    """home route serving the default page"""
+    return render_template("0-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
